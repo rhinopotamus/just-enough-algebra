c07c631f27137898686ff00c23b472a8982c35f9
rhinopotamus <orangefish@gmail.com>
2024-07-25 18:45:28 -0600

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,9 +1,9 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "frontmatter-2",
+  "id": "frontmatter-3",
   "level": "1",
-  "url": "frontmatter-2.html",
+  "url": "frontmatter-3.html",
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
